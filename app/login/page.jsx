bfbdf9d789535d939ed312d8c02663dbff8fa875
johnpiapian <johnpiapian@gmail.com>
2023-05-25 18:58:37 -0500

    Add login page

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,9 +1,19 @@
-const Home = () => {
+export const metadata = {
+    title: 'Login'
+  };
+  
+  const Home = () => {
     return (
-        <section className="w-full flex-center flex-col">
-            <h1 className="text-center ">Login</h1>
-        </section>
-    )
-}
-
-export default Home;
+      <section className="w-full h-screen flex items-center justify-center bg-gray-100">
+        <div className="bg-white p-8 rounded shadow-lg">
+          <h1 className="text-3xl font-bold text-center mb-4">Login</h1>
+          <button className="w-full bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600 transition-colors">
+            Sign In with Google
+          </button>
+        </div>
+      </section>
+    );
+  };
+  
+  export default Home;
+  
--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,28 @@
+"use client";
+
+import { useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+
 const Home = () => {
+    const router = useRouter();
+    const { data: session, status } = useSession({
+        required: true,
+        onUnauthenticated() {
+            router.push('/login');
+        }
+    });
+
     return (
         <section className="w-full flex-center flex-col">
-            <h1>Home</h1>
+            {/* {status === 'loading' && <h1 className="text-center">Loading...</h1>} */}
+            {session && (
+                <>
+                    <h1 className="text-center ">Welcome {session.user.name}</h1>
+                </>
+            )}
         </section>
-    )
-}
+    );
+};
 
 export default Home;

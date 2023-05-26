f0d2db6d7b1c561eb6fff0608a67e96bc5766b59
johnpiapian <johnpiapian@gmail.com>
2023-05-25 21:40:39 -0500

    Fix flicker on logout

--- app/logout/page.jsx
+++ app/logout/page.jsx
@@ -5,13 +5,13 @@ import { signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 const Home = () => {
-    const { data: session } = useSession();
+    const { data: session, status } = useSession();
     const router = useRouter();
 
     useEffect(() => {
-        if (session?.user) signOut({ redirect: false});
-        router.push("/login");
-    }, [session, router]);
+        if(session?.user) signOut({ callbackUrl: "/login" });
+        else router.push("/login");
+    }, []);
 
     return (
         <section className="w-full h-screen flex items-center justify-center bg-gray-100"></section>

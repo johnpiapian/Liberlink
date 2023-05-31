e873dd948d9d3dfcb55230eb23a94dee2dc29a03
johnpiapian <johnpiapian@gmail.com>
2023-05-31 14:50:30 -0500

    Update API

--- app/api/link/route.js
+++ app/api/link/route.js
@@ -1,13 +1,22 @@
-
 import Link from "@models/link";
 import { connectToDB } from "@utils/database";
+
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@app/api/auth/[...nextauth]/route";
 
 
 export const GET = async (request, { params }) => {
     try {
+        const session = await getServerSession(authOptions);
+        
+        if(!session?.user?.id || !session?.user?.email) {
+            return new Response("Unauthorized", { status: 401 });
+        }
+
         await connectToDB();
 
-        const links = await Link.find();
+        // find links where ownerId matches the current user's id
+        const links = await Link.find({ ownerId: session.user.id });
 
         return new Response(JSON.stringify(links), { status: 200 });
     } catch (error) {
